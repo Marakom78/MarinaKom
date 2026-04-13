2056a9d18f3f21f9de0a575fee4a40408be45563
Student <student@ksipt.ru>
2026-04-13 10:46:19 +0300

    commit 13.04

--- controllers/article_controller.py
+++ controllers/article_controller.py
@@ -1,8 +1,12 @@
+import cgi
+
 from controllers.controller import Controller
 from models.article import Article
 from models.user import User
 from exceptions import NotFoundException
 from exceptions import UnauthorizedException
+from exceptions import InvalidArgumentException
+
 
 class ArticlesController(Controller):
    def index (self, request, response):
@@ -65,9 +69,28 @@ class ArticlesController(Controller):
       response.headers = [('Location', '/articles')]
 
    def add(self, request, response):
-      article = Article()
-      article.set_author_id(1)
-      article.set_name('Новая статья')
-      article.set_text('Текст новой статьи')
-      
-      article.save() 
+      if self.user is None:
+         raise UnauthorizedException('Необходимо авторизоваться')
+
+      response.text = self.view.render_html('articles/add.html', {'title' : 'Добавление статьи'})
+      if request.method == 'POST':
+         try:
+            form = cgi.FieldStorage(fp=request.environ['wsgi.input'], environ = request.environ)
+            fields = {
+               'name' : form.getvalue('name'),
+               'text' : form.getvalue('text'),
+            }
+            img_file = form['img']
+            article =Article.create(fields, img_file, self.user)
+            if isinstance(article, Article):
+               response.status_code = 302
+               response.headers = [('Location', '/articles')]
+               return
+
+         except InvalidArgumentException as e:
+            response.text = self.view.render_html('articles/add.html', {'title' : 'Добавление статьи', 'article_data' : fields, 'error' : e})
+            return         
+      response.text = self.view.render_html('articles/add.html', {'title': 'Добавление статьи'})
+
+
+         
--- models/article.py
+++ models/article.py
@@ -1,5 +1,10 @@
+import os
+
 from models.active_record_entity import ActiveRecordEntity
+from exceptions import InvalidArgumentException
 from models.user import User
+
+MAX_FILE_SIZE = 5 * 1024 * 1024
 
 class Article(ActiveRecordEntity):
   # __tablename__ = 'table1'
@@ -36,6 +41,72 @@ class Article(ActiveRecordEntity):
     self._text = text
   # def set_created_at(self, created_at):
   #   self._created_at = created_at
+
+  @staticmethod
+  def create(fields, img_file, author):
+      if not fields['name']:
+          raise InvalidArgumentException('не передано название статьи')
+
+      if not fields['text']:
+          raise InvalidArgumentException('не передан текст статьи')
+
+      if __class__.check_file_size(img_file, MAX_FILE_SIZE)[0] == False :
+          raise InvalidArgumentException('Слишком большой файл! Должно быть не более 5МБ')
+
+
+      article = Article()
+      article._name = fields['name']
+      article._text = fields['text']
+      article._author_id = author.get_id()
+
+      if img_file.filename:
+          file_path = 'uploads/' +  img_file.filename
+          article._img = file_path
+          os.makedirs('/uploads', exist_ok=True)
+          with open(file_path, 'wb') as f:
+              while True:
+                  chunk = img_file.file.read(8192)  # Читаем файл по частям
+                  if not chunk:
+                      break
+                  f.write(chunk)
+
+      article.save()
+      return article
+
+
+
+
+  @staticmethod
+  def check_file_size(file_item, max_size):
+      """
+      Проверка размера файла без загрузки всего файла в память.
+      Читает файл по частям и суммирует их размер.
+      """
+      total_size = 0
+      chunk_size = 8192  # Читаем по 8KB
+
+      # Сохраняем текущую позицию, чтобы потом вернуться
+      current_pos = file_item.file.tell()
+
+      try:
+          # Перемещаемся в начало файла
+          file_item.file.seek(0)
+
+          # Читаем файл по частям и суммируем размер
+          while True:
+              chunk = file_item.file.read(chunk_size)
+              if not chunk:
+                  break
+              total_size += len(chunk)
+
+              # Если уже превысили лимит, можно прервать проверку
+              if total_size > max_size:
+                  break
+      finally:
+          # Возвращаемся на исходную позицию для последующего чтения
+          file_item.file.seek(current_pos)
+      print(total_size)
+      return total_size <= max_size, total_size
 
   @staticmethod
   def get_table_name():
